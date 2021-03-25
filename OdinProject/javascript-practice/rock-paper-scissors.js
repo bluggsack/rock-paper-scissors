318394483cfb1c15b7425fa4d363798904725895
Carson Grecco <carsongrecco1@gmail.com>
2021-03-24 21:38:03 -0400

    Started writing the beginning code for the rock paper scissors assignment in The Odin Project

--- OdinProject/javascript-practice/rock-paper-scissors.js
+++ OdinProject/javascript-practice/rock-paper-scissors.js
@@ -1,2 +1,26 @@
 //Rock, paper scissors
-console.log("hello world!");
+//user plays against computer
+//user types in "rock", "paper", or "scissors"
+// computer randomly picks "rock", "paper", or "scissors"
+//decide who wins -- paper beats rock -- rock beats scissors -- scissors beats paper -- same option is a tie
+// print the results
+
+function computerPlays(){
+    let computerOption = Math.floor(Math.random() * 3) + 1;
+    if(computerOption === 1){
+        return "Rock"
+    }
+    else if(computerOption === 2){
+        return "Paper"
+    }
+    else{
+        return "Scissors"
+    }
+};
+function playRPS(userChoice, computerChoice){
+    if(userChoice === computerChoice){
+        return "It's a tie! Go again."
+    }
+};
+console.log(computerPlays());
+console.log(playRPS("Rock", "Rock"));
