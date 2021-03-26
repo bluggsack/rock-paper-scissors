3d6b62077cbdbf48f3c58f3529d642bd572245b7
Carson Grecco <carsongrecco1@gmail.com>
2021-03-26 01:27:17 -0400

    Added the code for the first round of Rock Paper Scissors. Need to figure out a better way to write it and need to figure out how to make it case insensitive.

--- OdinProject/javascript-practice/rock-paper-scissors.js
+++ OdinProject/javascript-practice/rock-paper-scissors.js
@@ -8,19 +8,43 @@
 function computerPlays(){
     let computerOption = Math.floor(Math.random() * 3) + 1;
     if(computerOption === 1){
-        return "Rock"
+        return "rock"
     }
     else if(computerOption === 2){
-        return "Paper"
+        return "paper"
     }
     else{
-        return "Scissors"
+        return "scissors"
     }
 };
+
 function playRPS(userChoice, computerChoice){
-    if(userChoice === computerChoice){
+    //user chooses rock
+    if(userChoice === "rock" && computerChoice === "rock"){
         return "It's a tie! Go again."
+    }else if(userChoice === "rock" && computerChoice === "paper"){
+        return "You lose! Paper beats rock!"
+    }else if(userChoice === "rock" && computerChoice === "scissors"){
+        return "You win! Rock beats scissors!"
+    }//user chooses scissors
+    else if(userChoice === "scissors" && computerChoice === "scissors"){
+        return "It's a tie! Go again."
+    }else if(userChoice === "scissors" && computerChoice === "paper"){
+        return "You win! Scissors beats paper!"
+    }else if(userChoice === "scissors" && computerChoice === "rock"){
+        return "You lose! Rock beats scissors!"
+    }//user chooses paper
+    else if(userChoice === "paper" && computerChoice === "paper"){
+        return "It's a tie! Go again."
+    }else if(userChoice === "paper" && computerChoice === "rock"){
+        return "You win! Paper beats rock!"
+    }else if(userChoice === "paper" && computerChoice === "scissors"){
+        return "You loser! Scissors beats paper!"
     }
 };
-console.log(computerPlays());
-console.log(playRPS("Rock", "Rock"));
+
+const userChoice = (prompt("Let's play Rock, Paper, Scissors! What will you go with?"));
+const computerChoice = computerPlays();
+console.log(playRPS(userChoice.toLowerCase, computerChoice.toLowerCase));
+console.log(userChoice);
+console.log(computerChoice);
