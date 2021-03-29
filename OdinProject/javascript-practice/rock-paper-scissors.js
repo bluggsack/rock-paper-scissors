a04f740cd2e8e8b4921a6871aefaaf960766d896
Carson Grecco <carsongrecco1@gmail.com>
2021-03-28 22:53:15 -0400

    Was able to figure out how to get the first round to work. Still need to figure out the logic behind who wins and loses, as well as making it case insensitive. Decided to cut out the user choosing what they pick for right now just to be able to figure out the winning/losing aspect.

--- OdinProject/javascript-practice/rock-paper-scissors.js
+++ OdinProject/javascript-practice/rock-paper-scissors.js
@@ -1,3 +1,15 @@
+/*
+-------
+MY PLAN
+-------
+- Ask the user if they would like to play rock paper scissors.
+- If yes, ask the user what choice they will go with (rock, paper, scissors)
+- Get the computers choice via a random number that is associated with either Rock, paper, or scissors.
+- Compare the users and computers choice. Decide who wins by following the rules of the game.
+- Output whoever won the round, first to 5 round wins is the winner of the game.
+- Return to the beginning of the game.
+*/
+
 //Rock, paper scissors
 //user plays against computer
 //user types in "rock", "paper", or "scissors"
@@ -5,20 +17,60 @@
 //decide who wins -- paper beats rock -- rock beats scissors -- scissors beats paper -- same option is a tie
 // print the results
 
-function computerPlays(){
-    let computerOption = Math.floor(Math.random() * 3) + 1;
+/*
+------------------
+What i need to fix
+------------------
+- when the user picks paper and the computer picks rock, the user still loses, even though they should win.
+        * User-paper = 2, computer-rock = 1
+- when the user picks rock and the computer picks paper, the user wins, when they should lose.
+        * User-rock = 1, computer-paper = 2
+- when the user picks paper and the computer picks scissors, the user wins, when they should lose.
+        * User-paper = 2, computer-scissors = 3
+- when the user picks scissors and the computer picks paper, the user loses, when they should win.
+        * User-scissors = 3, computer-paper = 2
+*/
+
+let computerOption = Math.floor(Math.random() * 3) + 1;
+let userOption = Math.floor(Math.random() * 3) + 1;
+
+function computerPlay(){
+   
     if(computerOption === 1){
         return "rock"
     }
     else if(computerOption === 2){
         return "paper"
     }
-    else{
+    else if(computerOption === 3){
         return "scissors"
     }
 };
 
-function playRPS(userChoice, computerChoice){
+function userPlay(){
+    
+    if(userOption === 1){
+        return "rock"
+    }
+    else if(userOption === 2){
+        return "paper"
+    }
+    else if(userOption === 3){
+        return "scissors"
+    }
+};
+
+function playRockPaperScissors(playerSelection, computerSelection){
+    //const playerSelection = prompt("Let's play Rock, Paper, Scissors! What would you like to go with?");
+
+    if(computerOption === userOption){
+        return "It's a tie! Let's try that again."
+    }else if(computerOption < userOption){
+        return "You lose! Better luck next time!"
+    }else{
+        return "You win! Good job!"
+    }
+    /*
     //user chooses rock
     if(userChoice === "rock" && computerChoice === "rock"){
         return "It's a tie! Go again."
@@ -41,10 +93,9 @@ function playRPS(userChoice, computerChoice){
     }else if(userChoice === "paper" && computerChoice === "scissors"){
         return "You loser! Scissors beats paper!"
     }
+    */
 };
 
-const userChoice = (prompt("Let's play Rock, Paper, Scissors! What will you go with?"));
-const computerChoice = computerPlays();
-console.log(playRPS(userChoice.toLowerCase, computerChoice.toLowerCase));
-console.log(userChoice);
-console.log(computerChoice);
+console.log(playRockPaperScissors(userPlay, computerPlay));
+console.log(userPlay());
+console.log(computerPlay());
